f447e1a2cedcd728dc65c2fbd09f80abc06286b1
M53S-G656\typeo <typeofb@gmail.com>
2022-03-22 07:40:01 +0900

    테이블 비정규화로 삽입

--- sonde.py
+++ sonde.py
@@ -14,46 +14,43 @@ def db_transfer():
 
     if not os.path.exists(root_path + "/logs"):
         os.makedirs(root_path + "/logs")
-    log_path = root_path + \
-        "/logs/UPLOAD_{}.log".format(datetime.now().strftime("%Y%m%d"))
+    log_path = root_path + "/logs/UPLOAD_{}.log".format(datetime.now().strftime("%Y%m%d"))
     log_dt_format = "%Y-%m-%d %H:%M:%S"
 
     try:
         with open(root_path + '/sonde_2020061600.txt', 'r') as reader:
-            mst_list = []
-            dtl_list = []
+            result_list = []
 
             for i, line in enumerate(reader.readlines()):
+                # header skip
                 if i < 3:
-                    continue  # 헤더 스킵
-                # if i > 155: continue
+                    continue
+                # if i > 200:
+                #     continue
                 words = re.split(r'\s+', line.strip())
-                if len(words) < 9:  # hierarchy level 구분
-                    mst_list.append(words)
+                # hierarchy level 1
+                if len(words) < 9:
+                    mst_list = []
+                    mst_list.extend(words)
                 else:
-                    dtl_list.append(words)
+                    result_list.append(mst_list + words)
+                    # print(result_list)
 
-            # df_mst = pd.DataFrame(mst_list)
-            # df_mst.columns = ["nobs", "type", "StnID", "lat", "lon", "StnHgt", "nlev", "ObsTime"]
-            # write_mysql(df_mst)
-
-            df_dtl = pd.DataFrame(dtl_list)
-            df_dtl.columns = ["lev", "Pressure", "Height", "GPM", "T", "Td",
-                              "RH", "Wd", "Ws", "u", "v", "q", "vsign", "vlon", "vlat", "time"]
-            database.write_mysql('sonde_dtl', df_dtl)
+            df_dtl = pd.DataFrame(result_list)
+            df_dtl.columns = ["nobs", "type", "StnID", "lat", "lon", "StnHgt", "nlev", "ObsTime",
+                              "lev", "Pressure", "Height", "GPM", "T", "Td", "RH", "Wd", "Ws", "u", "v", "q", "vsign", "vlon", "vlat", "time"]
+            database.write_mysql('sonde', df_dtl)
 
             logger.upload_info(
                 "Successfully uploaded the file {}".format("sonde_2020061600.txt"))
             with open(log_path, "a") as writer:
-                writer.write("[INFO] [UPLOAD] [{}] Successfully uploaded the file {}\n".format(
-                    datetime.now().strftime(log_dt_format), "sonde_2020061600.txt"))
+                writer.write("[INFO] [UPLOAD] [{}] Successfully uploaded the file {}\n".format(datetime.now().strftime(log_dt_format), "sonde_2020061600.txt"))
     except Exception as e:
         print("File not accessible")
 
         logger.upload_error(e)
         with open(log_path, "a") as writer:
-            writer.write("[ERROR] [UPLOAD] [{}] {}\n".format(
-                datetime.now().strftime(log_dt_format), e))
+            writer.write("[ERROR] [UPLOAD] [{}] {}\n".format(datetime.now().strftime(log_dt_format), e))
 
 
 if __name__ == '__main__':
