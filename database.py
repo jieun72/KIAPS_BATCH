1f53b31dc51466c9321c00f0052dbd5d17f85e94
M53S-G656\typeo <typeofb@gmail.com>
2022-03-23 07:03:47 +0900

    no message

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 
 
 def write_mysql(table_name, df):
-    connection_url = r'mysql+pymysql://livingmap:wAps!06172019@livingmap.co.kr:3306/wmolcdb_new?charset=utf8'
+    connection_url = r"mysql+pymysql://livingmap:wAps!06172019@livingmap.co.kr:3306/wmolcdb_new?charset=utf8"
     engine = create_engine(connection_url)
     try:
         with engine.begin() as conn:
-            df.to_sql(name=table_name, con=engine, if_exists='replace', index=False)
+            df.to_sql(name=table_name, con=engine, if_exists="replace", index=False)
             print(">>> All good.")
     except Exception as e:
         print(">>> Something went wrong: " + e)
