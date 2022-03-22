ac4a1e1459d63d39b8f71c5938a29e4724e43b1b
M53S-G656\typeo <typeofb@gmail.com>
2022-03-22 20:58:17 +0900

    컬럼 타입 변경

--- sonde.py
+++ sonde.py
@@ -18,7 +18,7 @@ def db_transfer():
     log_dt_format = "%Y-%m-%d %H:%M:%S"
 
     try:
-        with open(root_path + '/sonde_2020061600.txt', 'r') as reader:
+        with open(root_path + "/sonde_2020061600.txt", "r") as reader:
             result_list = []
 
             for i, line in enumerate(reader.readlines()):
@@ -27,7 +27,7 @@ def db_transfer():
                     continue
                 # if i > 200:
                 #     continue
-                words = re.split(r'\s+', line.strip())
+                words = re.split(r"\s+", line.strip())
                 # hierarchy level 1
                 if len(words) < 9:
                     mst_list = []
@@ -39,7 +39,32 @@ def db_transfer():
             df = pd.DataFrame(result_list)
             df.columns = ["nobs", "type", "StnID", "lat", "lon", "StnHgt", "nlev", "ObsTime",
                           "lev", "Pressure", "Height", "GPM", "T", "Td", "RH", "Wd", "Ws", "u", "v", "q", "vsign", "vlon", "vlat", "time"]
-            database.write_mysql('sonde', df)
+            df["nobs"] = df["nobs"].astype(int)
+            df["type"] = df["type"].astype(int)
+            df["StnID"] = df["StnID"].astype(str)
+            df["lat"] = df["lat"].astype(float)
+            df["lon"] = df["lon"].astype(float)
+            df["StnHgt"] = df["StnHgt"].astype(float)
+            df["nlev"] = df["nlev"].astype(int)
+            df["ObsTime"] = df["ObsTime"].astype(str)
+            df["lev"] = df["lev"].astype(int)
+            df["Pressure"] = df["Pressure"].astype(float)
+            df["Height"] = df["Height"].astype(float)
+            df["GPM"] = df["GPM"].astype(float)
+            df["T"] = df["T"].astype(float)
+            df["Td"] = df["Td"].astype(float)
+            df["RH"] = df["RH"].astype(float)
+            df["Wd"] = df["Wd"].astype(float)
+            df["Ws"] = df["Ws"].astype(float)
+            df["u"] = df["u"].astype(float)
+            df["v"] = df["v"].astype(float)
+            df["q"] = df["q"].astype(float)
+            df["vsign"] = df["vsign"].astype(float)
+            df["vlon"] = df["vlon"].astype(float)
+            df["vlat"] = df["vlat"].astype(float)
+            df["time"] = df["time"].astype(float)
+            print(df.dtypes)
+            database.write_mysql("sonde", df)
 
             logger.upload_info("Successfully uploaded the file {}".format("sonde_2020061600.txt"))
             with open(log_path, "a") as writer:
@@ -52,5 +77,5 @@ def db_transfer():
             writer.write("[ERROR] [UPLOAD] [{}] {}\n".format(datetime.now().strftime(log_dt_format), e))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     db_transfer()
