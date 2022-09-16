3eaf4f84fd71c9a0eeb37dc16abd0304693ac830
jieun72 <lee.jieun@kesti.co.kr>
2022-09-16 16:28:25 +0900

    220916 SONDE_InnoQC 구현

--- modules/sonde/sonde_innqc.py
+++ modules/sonde/sonde_innqc.py
@@ -1,6 +1,7 @@
 import re
 
 import pandas as pd
+import numpy as np
 
 from libs import database
 from libs.kiapslogging import KiapsLogging
@@ -35,39 +36,63 @@ class SondeInnQC(MainBase):
                     result_list.append(mst_list + words)
 
             df = pd.DataFrame(result_list)
-            df.columns = ["tobs", "StnID", "obtype", "nlev", "lat", "lon", "StnHgt", "ObsTime",
-                          "lev", "Pressure", "Temp", "Temp_flag", "Wd", "Wd_flag", "Ws", "Ws_flag", "U", "U_flag",
-                          "V", "V_flag", "Rh", "Rh_flag", "Q", "Q_flag", "z", "z_flag",
-                          "Mflag", "avail"]
-            df.insert(0, "datetime", self.config.get("GLOBAL", "FILE_DATE"))
+            df.columns = ["nobs", "StnID", "type", "nlev", "lat", "lon", "StnHgt", "ObsTime",
+                          "lev", "Pressure", "T", "T_flag", "Wd", "Wd_flag", "Ws", "Ws_flag", "U", "U_flag",
+                          "V", "V_flag", "rh", "rh_flag", "q", "q_flag", "z", "z_flag",
+                          "mflag", "avail"]
+
+            df = df.drop([df.columns[8]], axis=1)                          
+            df.insert(0, "datetime", self.config.get("GLOBAL", "FILE_DATE")+'00')
+
             df["datetime"] = pd.to_datetime(df["datetime"], format="%Y%m%d%H%M%S")
-            df["tobs"] = df["tobs"].astype(int)
+            df["nobs"] = df["nobs"].astype(int)
             df["StnID"] = df["StnID"].astype(int)
-            df["obtype"] = df["obtype"].astype(str)
+            df["type"] = df["type"].astype(str)
             df["nlev"] = df["nlev"].astype(int)
             df["lat"] = df["lat"].astype(float)
             df["lon"] = df["lon"].astype(float)
             df["StnHgt"] = df["StnHgt"].astype(float)
-            df["ObsTime"] = df["ObsTime"].astype(float)
+            df["ObsTime"] = np.nan
 
-            df["lev"] = df["lev"].astype(int)
             df["Pressure"] = df["Pressure"].astype(float)
-            df["Temp"] = df["Temp"].astype(float)
-            df["Temp_flag"] = df["Temp_flag"].astype(float)
+            df["T"] = df["T"].astype(float)
+            df["T_flag"] = df["T_flag"].astype(float)
+            df.insert(12, "T_KPOP_QC", np.nan)
+            df.insert(13, "T_AI_QC", np.nan)
+
             df["Wd"] = df["Wd"].astype(float)
             df["Wd_flag"] = df["Wd_flag"].astype(float)
+            df.insert(16, "Wd_KPOP_QC", np.nan)
+            df.insert(17, "Wd_AI_QC", np.nan)
+
             df["Ws"] = df["Ws"].astype(float)
             df["Ws_flag"] = df["Ws_flag"].astype(float)
+            df.insert(20, "Ws_KPOP_QC", np.nan)
+            df.insert(21, "Ws_AI_QC", np.nan)
+
             df["U"] = df["U"].astype(float)
             df["U_flag"] = df["U_flag"].astype(float)
+            df.insert(24, "U_KPOP_QC", np.nan)
+            df.insert(25, "U_AI_QC", np.nan)
+
             df["V"] = df["V"].astype(float)
             df["V_flag"] = df["V_flag"].astype(float)
-            df["Rh"] = df["Rh"].astype(float)
-            df["Rh_flag"] = df["Rh_flag"].astype(float)
-            df["Q"] = df["Q"].astype(float)
-            df["Q_flag"] = df["Q_flag"].astype(float)
+            df.insert(28, "V_KPOP_QC", np.nan)
+            df.insert(29, "V_AI_QC", np.nan)
+
+            df["rh"] = df["rh"].astype(float)
+            df["rh_flag"] = df["rh_flag"].astype(float)
+            df.insert(32, "rh_KPOP_QC", np.nan)
+            df.insert(33, "rh_AI_QC", np.nan)
+
+            df["q"] = df["q"].astype(float)
+            df["q_flag"] = df["q_flag"].astype(float)
+            df.insert(36, "q_KPOP_QC", np.nan)
+            df.insert(37, "q_AI_QC", np.nan)
+
             df["z"] = df["z"].astype(float)
             df["z_flag"] = df["z_flag"].astype(float)
-            df["Mflag"] = df["Mflag"].astype(float)
+            df["mflag"] = df["mflag"].astype(float)
             df["avail"] = df["avail"].astype(float)
+
             database.write_mysql(file_name, df)
